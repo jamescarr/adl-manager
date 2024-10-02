e2793c9c09ea493debe1b8067ef6700392414af4
James Carr <james.r.carr@gmail.com>
2024-10-02 07:07:24 -0500

    Only users with the admin role can view or edit users

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -67,4 +67,24 @@ class UserResource extends Resource
             'edit' => Pages\EditUser::route('/{record}/edit'),
         ];
     }
+
+    public static function canViewAny(): bool
+    {
+        return auth()->user()->hasRole('Admin');
+    }
+
+    public static function canCreate(): bool
+    {
+        return auth()->user()->hasRole('Admin');
+    }
+
+    public static function canEdit($record): bool
+    {
+        return auth()->user()->hasRole('Admin');
+    }
+
+    public static function canDelete($record): bool
+    {
+        return auth()->user()->hasRole('Admin');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,14 +12,6 @@ Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
-    'role:Admin'])->prefix('admin')->group(function () {
-        Route::resource('/admin/users', UserResource::class);
-    });
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
